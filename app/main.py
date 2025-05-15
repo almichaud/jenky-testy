d82a9b9f36b96e8df705de4228cf70b438b9445f
Audrey <audrey@Madisons-MacBook-Air.local>
2025-05-14 21:17:16 -0500

    pushing new message

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hewwo World"}
+    return {"message": "Hewwo World, and also I'm an avid consumer of big buts and I can not lie"}
 
